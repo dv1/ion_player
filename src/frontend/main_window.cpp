d067fa610846308463ec6dcb39aae96abd204e4e
Carlos Rafael Giani <dv@pseudoterminal.org>
2010-08-05 12:17:59 +0200

    Added build files to mpg123

---
+++ src/frontend/main_window.cpp
@@ -0,0 +1,24 @@
+#include "main_window.hpp"
+#include "ui_main_window.h"
+#include "ui_position_volume_widget.h"
+
+
+namespace ion
+{
+namespace frontend
+{
+
+
+main_window::main_window()
+{
+}
+
+
+main_window::~main_window()
+{
+}
+
+
+}
+}
+
